f4cb44204b68fad0d4f03051edab3ccfd38c3926
youngkwon02 <youngkwon02@naver.com>
2022-07-24 01:11:24 +0900

    [FEAT] @Query DTO searching test

--- src/main/java/com/spring/data/datajpa/repository/MemberRepository.java
+++ src/main/java/com/spring/data/datajpa/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.spring.data.datajpa.repository;
 
+import com.spring.data.datajpa.dto.MemberDto;
 import com.spring.data.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new com.spring.data.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
--- src/test/java/com/spring/data/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/spring/data/datajpa/repository/MemberRepositoryTest.java
@@ -1,20 +1,18 @@
 package com.spring.data.datajpa.repository;
 
+import com.spring.data.datajpa.dto.MemberDto;
 import com.spring.data.datajpa.entity.Member;
-import org.junit.jupiter.api.Assertions;
+import com.spring.data.datajpa.entity.Team;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -23,6 +21,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -88,5 +89,37 @@ class MemberRepositoryTest {
 
         List<Member> result = memberRepository.findUser("AAA", 10);
         assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUsernameList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for(String s : usernameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team teamA = new Team("teamA");
+        teamRepository.save(teamA);
+        Member m1 = new Member("AAA", 10, teamA);
+        memberRepository.save(m1);
+
+
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamB);
+        Member m2 = new Member("BBB", 20, teamB);
+        memberRepository.save(m2);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
     }
 }
