6b114e27280e42a22371ff5200f03a19c5b8f9c8
youngkwon02 <youngkwon02@naver.com>
2022-07-24 00:43:41 +0900

    [FEAT] @Query at Repository method

--- src/main/java/com/spring/data/datajpa/repository/MemberRepository.java
+++ src/main/java/com/spring/data/datajpa/repository/MemberRepository.java
@@ -2,9 +2,14 @@ package com.spring.data.datajpa.repository;
 
 import com.spring.data.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    public List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
