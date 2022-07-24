b19829b1a35720187d1bbc467d5f425eff69a6d0
youngkwon02 <youngkwon02@naver.com>
2022-07-24 17:06:13 +0900

    [FEAT] Fetch Join using EntityGraph annotation

--- src/main/java/com/spring/data/datajpa/repository/MemberRepository.java
+++ src/main/java/com/spring/data/datajpa/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import com.spring.data.datajpa.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -45,4 +46,15 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true) // executeUpdate 같은 Annotation
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    // Entity Graph를 쓰면 Query를 직접 작성하지 않고 Fetch Join 가능
+//    @Query("select m from Member m left join fetch m.team")
+//    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 }
--- src/test/java/com/spring/data/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/spring/data/datajpa/repository/MemberRepositoryTest.java
@@ -241,4 +241,32 @@ class MemberRepositoryTest {
         // then
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when
+
+        List<Member> members = memberRepository.findAll();
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.teamClass = " + member.getTeam().getClass());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+
+        // then
+    }
 }
